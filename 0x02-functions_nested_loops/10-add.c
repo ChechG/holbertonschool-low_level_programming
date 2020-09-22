cc2cdd211fd748b5898765c866cc46c4683d5138
ChechG <2100@holbertonschool.com>
2020-09-22 18:07:47 +0000

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - check the code for Holberton School students.
+ * @a: first integer
+ * @b: second integer
+ * Return: @n.
+ */
+int add(int a, int b)
+{
+int n = a + b;
+return (n);
+}
