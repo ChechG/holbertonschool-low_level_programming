bf04954044c5425be22272593adaf1313808651a
ChechG <2100@holbertonschool.com>
2020-09-22 13:03:30 +0000

    3 comm

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,10 @@
 #include "holberton.h"
+
+/**
+ * _islower - check the code for Holberton School students.
+ * @c: to be determined
+ * Return: 1 or 2.
+ */
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
