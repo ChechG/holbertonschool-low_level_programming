7ff177001417a25901489b64edd82f37067d94c4
ChechG <2100@holbertonschool.com>
2020-09-22 12:22:59 +0000

    0 comment 2

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -4,13 +4,14 @@
  *
  * Return: Always 0.
  */
-int main(void){
-  char a[9] = "Holberton";
-  int b;
-  for (b = 0; b < 9; b++)
-    {
-      _putchar(a[b]);
-    }
-  _putchar('\n');
-  return(0);
+int main(void)
+{
+char a[9] = "Holberton";
+int b;
+for (b = 0; b < 9; b++)
+{
+_putchar(a[b]);
+}
+_putchar('\n');
+return (0);
 }
