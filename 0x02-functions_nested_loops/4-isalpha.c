7ce38e9d951c3f3b5ab4c6dd794360f40f344928
ChechG <2100@holbertonschool.com>
2020-09-22 13:07:17 +0000

    4 comment

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,9 @@
 #include "holberton.h"
+/**
+ * _isalpha - check the code for Holberton School students.
+ * @c: local variable to be determinded
+ * Return: Always 0.
+ */
 int _isalpha(int c)
 {
 if ((c >= 65 && c <= 90)  || (c >= 97 && c <= 122))
