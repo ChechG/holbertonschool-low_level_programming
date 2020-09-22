ab853e8658a84b3f62efd1ef6b300509c0f92e1b
ChechG <2100@holbertonschool.com>
2020-09-22 13:09:51 +0000

    5 comment

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,9 @@
 #include "holberton.h"
+/**
+ * print_sign - check the code for Holberton School students.
+ * @n: local variable to be determinded
+ * Return: 0, 1 or -1.
+ */
 int print_sign(int n)
 {
 if (n == 0)
