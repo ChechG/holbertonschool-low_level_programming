438b287dd0773e40a5f133b67ebad73249f5ad05
ChechG <2100@holbertonschool.com>
2020-09-23 22:48:52 +0000

    102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * fibonacci - check the code for Holberton School students.
+ * Return: void.
+ */
+void fibonacci(void)
+{
+signed long int a, fib, fib_ant, fib2;
+fib = 1;
+fib_ant = 0;
+for (a = 0; a <= 48; a++)
+{
+fib2 = fib + fib_ant;
+printf("%ld", fib2);
+fib_ant = fib;
+fib = fib2;
+if (a != 48)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
+/**
+ * main - check the code for Holberton School students.
+ * Return: void.
+ */
+int main(void)
+{
+fibonacci();
+return (0);
+}
