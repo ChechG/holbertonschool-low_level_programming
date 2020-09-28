152509e9b4f66fdbf56a4e249d5a3b0253480040
ChechG <2100@holbertonschool.com>
2020-09-28 17:14:32 +0000

    my first command

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - check the code for Holberton School students.
+ * *n: direction to int to be determined.
+ * Return: void.
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
