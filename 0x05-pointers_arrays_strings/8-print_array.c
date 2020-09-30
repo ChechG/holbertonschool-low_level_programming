b56ed8eaa90a26f71ff80a5854c36a9ebbb5593b
ChechG <2100@holbertonschool.com>
2020-09-30 16:37:06 +0000

    delete

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 #include <stdio.h>
-#include <string.h>
 /**
  * print_array - check the code for Holberton School students.
  * @a: array of ints to be determined.
@@ -10,6 +9,7 @@
 void print_array(int *a, int n)
 {
 int b;
+
 for (b = 0; b < n; b++)
 {
 printf("%d", a[b]);
