33401d15e8db728d30aa617fc5c19a64a75723bc
ChechG <2100@holbertonschool.com>
2020-10-01 15:49:32 +0000

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,4 @@
 #include "holberton.h"
-/**
- * _strlen - check the code for Holberton School students.
- * @s: pinter to int to be determined.
- * Return: int.
- */
-int _strlen(char *s)
-{
-	int n = 0;
-
-	while (s[n] != '\0')
-	{
-		n++;
-	}
-	return (n);
-}
 
 /**
  * *_strncpy - check the code for Holberton School students.
