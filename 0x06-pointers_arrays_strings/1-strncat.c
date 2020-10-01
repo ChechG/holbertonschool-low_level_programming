30513d78366245bdfd25160101adc6a0d4be1cbb
ChechG <2100@holbertonschool.com>
2020-10-01 12:54:11 +0000

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: pinter to int to be determined.
+ * Return: int.
+ */
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	return (n);
+}
+
+/**
+ * *_strncat - check the code for Holberton School students.
+ * @dest: char pointer to be determined.
+ * @src: char pointre to be determined.
+ * @n: int to be determined.
+ * Return: char.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, i;
+
+	a = _strlen(dest);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[a + i] = src[i];
+	}
+	return (dest);
+}
