1a6575100904da768fa64b5d769d2b78d594e31e
ChechG <2100@holbertonschool.com>
2020-10-02 00:26:03 +0000

    on

---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void print_buffer(char *b, int size)
+{
+	int cont = 0, i, i2;
+	for (i = 0; i <= size; i += 10)
+	{
+		printf("%08x: ", cont);
+		printf("%02x%02x ", b[i], b[i + 1]);
+		printf("%02x%02x ", b[i + 2], b[i + 3]);
+		printf("%02x%02x ", b[i + 4], b[i + 5]);
+		printf("%02x%02x ", b[i + 6], b[i + 7]);
+
+		cont = cont + 10;
+		if(i % 10 == 0)
+		{
+			putchar('\n');
+		}
+	}
+}
