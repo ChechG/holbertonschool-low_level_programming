2b4038dbc1dbfe668dc15460265aaabbe37fae9b
ChechG <2100@holbertonschool.com>
2020-10-02 01:26:38 +0000

    o

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -8,32 +8,40 @@
  */
 void print_buffer(char *b, int size)
 {
-	int cont = 0, i, i2;
-
-	for (i = 0; i <= size; i += 10)
+	if (size > 0)
 	{
-		printf("%08x: ", cont);
-		printf("%02x%02x ", b[i], b[i + 1]);
-		printf("%02x%02x ", b[i + 2], b[i + 3]);
-		printf("%02x%02x ", b[i + 4], b[i + 5]);
-		printf("%02x%02x ", b[i + 6], b[i + 7]);
-		cont = cont + 10;
-		for (i2 = cont; i2 < cont + 10; i2 += 10)
+		int cont = 0, i, i2, i3;
+		i3 = 0;
+		int p = 0;
+		for (i = 0; i < size; i += 10)
 		{
-			putchar(b[i2]);
-			putchar(b[i2 + 1]);
-			putchar(b[i2 + 2]);
-			putchar(b[i2 + 3]);
-			putchar(b[i2 + 4]);
-			putchar(b[i2 + 5]);
-			putchar(b[i2 + 6]);
-			putchar(b[i2 + 7]);
-			putchar(b[i2 + 8]);
-			putchar(b[i2 + 9]);
-		}
-		if(i % 10 == 0)
-		{
-			putchar('\n');
+			printf("%08x: ", cont);
+			printf("%02x%02x ", b[i], b[i + 1]);
+			printf("%02x%02x ", b[i + 2], b[i + 3]);
+			printf("%02x%02x ", b[i + 4], b[i + 5]);
+			printf("%02x%02x ", b[i + 6], b[i + 7]);
+			printf("%02x%02x ", b[i + 8], b[i + 9]);
+			for (i2 = cont; i2 < cont + 10; i2 += 10)
+			{
+				for (p = 0; p < 11; p++)
+				{
+					if (b[i + p] >= 0 && b[i + p] <= 32)
+					{
+						putchar(46);
+					}
+					else
+						putchar(b[i+ p]);
+				}
+
+			}
+
+			cont = cont + 10;
+			if(i % 10 == 0)
+			{
+				putchar('\n');
+			}
 		}
 	}
+	else
+		printf("\n");
 }
