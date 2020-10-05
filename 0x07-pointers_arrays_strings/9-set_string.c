e589fb7ea4e50a0d0d7eac9c30b7a6942a72e840
ChechG <2100@holbertonschool.com>
2020-10-05 21:01:23 +0000

    9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - check the code for Holberton School students.
+ * @s: char pointer to be determined.
+ * @to: char pointer to replace.
+ * Return: void.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
