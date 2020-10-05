6c08f3b3a17ade9519466f8454e20a3907e1928d
ChechG <2100@holbertonschool.com>
2020-10-05 21:06:37 +0000

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * _memset - check the code for Holberton School students.
@@ -12,7 +11,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	while (n > 0)
 	{
-		s = b;
+		*s = b;
 		s++;
 		n--;
 	}
