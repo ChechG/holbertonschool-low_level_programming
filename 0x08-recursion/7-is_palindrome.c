3cdae9f71bb19ce364666248db59d69067974118
ChechG <2100@holbertonschool.com>
2020-10-07 18:20:30 +0000

    7

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - check the code for Holberton School students.
+ * @s: char pointer to be determined.
+ * Return: int.
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s != '\0')
+	{
+		i = i + 1 + _strlen_recursion(s + 1);
+	}
+
+	return (i);
+}
+/**
+ * pali - check the code for Holberton School students.
+ * @ini: initial position.
+ * @fin: final position.
+ * @s: char pointer to be determined.
+ * Return: int.
+ */
+int pali(char *s, int ini, int fin)
+{
+	if (s[ini] != s[fin])
+	{
+		return (0);
+	}
+	else if (s[ini] == s[fin] && ini <= fin)
+	{
+		ini++;
+		fin--;
+		pali(s, ini, fin);
+	}
+
+	return (1);
+}
+/**
+ * is_palindrome - check the code for Holberton School students.
+ * @s: char pointer to be determined.
+ * Return: int.
+ */
+int is_palindrome(char *s)
+{
+	int i = _strlen_recursion(s);
+
+	return (pali(s, 0, i - 1));
+}
