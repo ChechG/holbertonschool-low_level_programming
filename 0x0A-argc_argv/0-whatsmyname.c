55e13beac081b37a3b08cf2889d4867da2a198de
ChechG <2100@holbertonschool.com>
2020-10-09 13:26:01 +0000

    my first command

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+	printf("%s\n", argv[0]);
+	}
+	return (0);
+}
