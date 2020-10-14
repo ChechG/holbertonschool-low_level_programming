951e5559e5b9138af7ac5c7e66efdb8bdafa4a89
ChechG <2100@holbertonschool.com>
2020-10-14 19:23:31 +0000

    0x0B

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - check the code for Holberton School students.
+ * @width: int to be determined.
+ * @height: int to be determined.
+ * Return: int.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **arr, i, j;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+
+	arr = (int **) malloc(height * sizeof(int *));
+	for (i = 0; i < height; i++)
+		arr[i] = (int *) malloc(width * sizeof(int));
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return (arr);
+}
