dd70034a419e1cd2b3bd7af620535b506fd03625
ChechG <2100@holbertonschool.com>
2020-10-14 19:36:08 +0000

    0x0B

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,6 +22,10 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = (int *) malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+		{
+			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 	{
