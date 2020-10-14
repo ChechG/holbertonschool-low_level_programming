e8d17d31b66f87b6381970449466d021d91a11a4
ChechG <2100@holbertonschool.com>
2020-10-14 22:45:19 +0000

    0x0B

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - check the code for Holberton School students.
+ * @ac: int pinter to be determined.
+ * @av: pointer to pointer to array of char.
+ * Return: char.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, k, j, cont;
+	char *quote;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; k < ac; k++)
+	{
+		for (j = 0; av[k][j] != '\0'; j++)
+		{
+			cont++;
+		}
+	}
+	cont += ac;
+	quote = (char *)malloc(cont * sizeof(char));
+	if (quote == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	for (k = 0; k < ac; k++)
+	{
+		for (j = 0; av[k][j] != '\0'; j++, i++)
+		{
+			quote[i] = av[k][j];
+		}
+		quote[i++] = '\n';
+	}
+	return (quote);
+}
