dbe7d4ddca38bee915fd9a5b8c5fdcb189d60dd5
ChechG <2100@holbertonschool.com>
2020-10-15 19:39:01 +0000

    0x0B

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _realloc - prints buffer in hexa
+ * @ptr: pointer of the memory size.
+ * @old_size: old size.
+ * @new_size: new size of memory.
+ * Return: void.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	ptr = malloc(new_size);
+
+	if (old_size == new_size)
+	{
+		return (ptr);
+	}
+	else if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	return (ptr);
+}
