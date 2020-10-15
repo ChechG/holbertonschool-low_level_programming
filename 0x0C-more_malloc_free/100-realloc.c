fe887afe8df7abc1bd035c8c8836d9b19b94e799
ChechG <2100@holbertonschool.com>
2020-10-15 19:52:55 +0000

    0x0B

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size < old_size)
 	{
-		free (ptr);
+		free(ptr);
 	}
 	ptr = malloc(new_size);
 	return (ptr);
