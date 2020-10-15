7c6bd7dd77a72ade0e8191ace10cfc283babad7d
ChechG <2100@holbertonschool.com>
2020-10-15 18:42:22 +0000

    0x0B

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/**
+ * array_range - check the code for Holberton School students.
+ * @min: int.
+ * @max: int.
+ * Return: int pointer.
+ */
 int *array_range(int min, int max)
 {
 	int i, largo;
