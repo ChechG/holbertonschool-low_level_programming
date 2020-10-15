d625a2d9f1a5a2c46b42675b7124a53549334491
ChechG <2100@holbertonschool.com>
2020-10-15 17:47:47 +0000

    0x0B

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *num;
 
-	num = (unsigned int *)malloc(nmemb * size * sizeof(unsigned int));
+	num = (int *)malloc(nmemb * size * sizeof(int));
 	if (num == NULL)
 	{
 		return(NULL);
