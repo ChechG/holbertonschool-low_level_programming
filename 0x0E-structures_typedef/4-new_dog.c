c5b8b77edca064a4873e6be8139c8e185554f4af
ChechG <2100@holbertonschool.com>
2020-10-19 20:18:33 +0000

    0x0D

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,27 @@
 #include "dog.h"
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * _strcpy - check the code for Holberton School students.
+ * @dest: char pointer to be determined.
+ * @src: char pointer to be determined.
+ * Return: 0 or 1.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+	int b;
+
+	while (src[a] != '\0')
+		a++;
+
+	for (b = 0; b <= a; b++)
+	{
+		dest[b] = src[b];
+	}
+	return (dest);
+}
 
 /**
  * new_dog - Short description
