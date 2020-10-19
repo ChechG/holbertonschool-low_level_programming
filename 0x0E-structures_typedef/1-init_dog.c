47fb757f755ddb7449b80bbb6c6ba0742087f22a
ChechG <2100@holbertonschool.com>
2020-10-19 17:24:15 +0000

    0x0D

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-
 /**
  * init_dog - check the code for Holberton School students.
  * @d: pointer to struct.
@@ -10,7 +9,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 }
