f04d43a2dc1528d60ffcb1c61c10147673dc823c
ChechG <2100@holbertonschool.com>
2020-10-24 15:25:54 +0000

    0x10

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
- * void print_numbers - check the code for Holberton School students.
+ * print_numbers - check the code for Holberton School students.
  * @separator: pointer to string.
  * @n: number of arguments
  * Return: void.
