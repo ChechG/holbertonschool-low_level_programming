41fdc7ba1c2758f411e37894607ca1f2f6575dc5
ChechG <2100@holbertonschool.com>
2020-10-24 15:29:50 +0000

    0x10

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,7 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list lista;
 
-	if (separator != NULL && n > 0)
+	if (separator != NULL)
 	{
 		va_start(lista, n);
 		for (i = 0; i < n; i++)
