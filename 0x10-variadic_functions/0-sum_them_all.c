0004d4adfbb9757de19f9fa7c7eedc15fe235679
ChechG <2100@holbertonschool.com>
2020-10-24 15:14:58 +0000

    0x10

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
-
+/**
+ * sum_them_all - sum.
+ * @n: unsigned int umber of arguments.
+ * Return: result.
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list lista;
