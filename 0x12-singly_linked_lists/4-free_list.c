e417f4a109e30cead5dce541afce2c0c27904518
ChechG <2100@holbertonschool.com>
2020-10-29 19:41:39 +0000

    0x12

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - frees the list space given..
+ * @head: list given.
+ * Return: void.
+ */
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+	head = NULL;
+}
