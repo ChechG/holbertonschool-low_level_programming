ff93cc52cfac9acebf1cd2b61fdd9463a5f920e0
ChechG <2100@holbertonschool.com>
2020-10-29 19:51:19 +0000

    0x12

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * premain - function.
+ * Return: void.
+ */
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
