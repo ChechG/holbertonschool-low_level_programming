43c107f2e508386d172e3cfd98fc01af740a4b9b
ChechG <2100@holbertonschool.com>
2020-11-04 13:05:37 +0000

    0x13

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,9 +11,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 	listint_t *nodo = head;
 
-	if (nodo == NULL)
+	if (head == NULL)
 		return (NULL);
-
 	for (i = 0; i < index; i++)
 	{
 		head = head->next;
