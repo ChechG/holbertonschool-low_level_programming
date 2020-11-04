c11b81af9ed0485c388615d1c816517bc362fe6c
ChechG <2100@holbertonschool.com>
2020-11-04 13:47:05 +0000

    0x13

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,23 +23,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		nodo->n = n;
-		nodo->next = temp;
-		temp = nodo;
+		nodo->next = (*head);
+		(*head) = nodo;
 		return (nodo);
 	}
 	for (i = 0; i < idx - 1 && temp != NULL; i++)
 	{
 		temp = temp->next;
 	}
-	if (temp != NULL)
-	{
-		nodo->n = n;
-		nodo->next = temp->next;
-		temp->next = nodo;
-	}
-	else
+	if (temp == NULL)
 	{
 		return (NULL);
 	}
+	nodo->n = n;
+	nodo->next = temp->next;
+	temp->next = nodo;
 	return (nodo);
 }
