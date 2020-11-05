a045910a70979512e0bf116b787a98a476cd8760
ChechG <2100@holbertonschool.com>
2020-11-05 18:44:28 +0000

    0x13

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_binary - converts and prints decimal to binary.
+ * @n: number to convert.
+ * Return: void.
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int m = n;
+	if (n > 1)
+	{
+		m = n & 1;
+		print_binary(n >> 1);
+	}
+	_putchar(m + '0');
+}
