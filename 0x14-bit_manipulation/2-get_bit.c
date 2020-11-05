efff8e91097237c8b7fd0649a99f41962ea4aaf8
ChechG <2100@holbertonschool.com>
2020-11-05 19:13:48 +0000

    0x13

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index.
+ * @n: number to traverse.
+ * @index: index number.
+ * Return: 1 or 0.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int m = 1;
+
+	m <<= index;
+	if ((m & n) != 0)
+		return (1);
+	else
+		return (0);
+	return (-1);
+}
