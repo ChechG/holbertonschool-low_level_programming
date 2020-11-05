1813d514c55742cd2e2ff8996943458026319bb2
ChechG <2100@holbertonschool.com>
2020-11-05 17:54:14 +0000

    0x13

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,36 +1,13 @@
 #include "holberton.h"
 /**
- * _pow_recursion - check the code for Holberton School students.
- * @x: int to be determined.
- * @y: int to be determined.
- * Return: int.
- */
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-	{
-		return (-1);
-	}
-	if (y == 0)
-	{
-		return (1);
-	}
-	if (y == 1)
-	{
-		return (x);
-	}
-
-	return (x * _pow_recursion(x, y - 1));
-}
-/**
  * binary_to_uint - convets binary to decimal.
  * @b: string of binary numbers.
  * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0, j = 0, potencia = 1, numero = 0, rem, dec = 0;
-	unsigned int c = 0;
+	unsigned int val = 0;
+	int i = 0;
 
 	if (b == NULL)
 		return (0);
@@ -38,23 +15,12 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] == '0' || b[i] == '1')
 		{
-			numero++;
+			val <<= 1;
+			val += b[i] - '0';
 			i++;
 		}
 		else
 			return (0);
 	}
-	for (i = numero - 1; i >= 0; i--)
-	{
-		c += (b[i] - '0') * potencia;
-		potencia *= 10;
-	}
-	while (c != 0)
-	{
-		rem = c % 10;
-		c /= 10;
-		dec += rem * _pow_recursion(2, j);
-		++j;
-	}
-	return (dec);
+	return (val);
 }
--- 0x14-bit_manipulation/0-main.c
+++ 0x14-bit_manipulation/0-main.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	unsigned int n;
 
-	n = binary_to_uint("1");
+	n = binary_to_uint("0");
 	printf("%u\n", n);
 	n = binary_to_uint("101");
 	printf("%u\n", n);
@@ -18,7 +18,7 @@ int main(void)
 	printf("%u\n", n);
 	n = binary_to_uint("1100010");
 	printf("%u\n", n);
-	n = binary_to_uint("0000000000000000000110010010");
+	n = binary_to_uint("1111111111");
 	printf("%u\n", n);
 	return (0);
 }
