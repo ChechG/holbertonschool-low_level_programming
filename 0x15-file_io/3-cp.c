21ef38be6ad0ee6dffb9a5c3e06a9ab535aad3c1
ChechG <2100@holbertonschool.com>
2020-11-09 20:02:05 +0000

    0x15

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,21 +16,19 @@ int main(int ac, char **av)
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fdto = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fdto = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 00664);
 	fdfrom = open(av[1], O_RDONLY);
-	buf = malloc(sizeof(char) * 1024);
-	rd = read(fdfrom, buf, 1023);
 	if (fdfrom == -1 || rd == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
+		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
 	}
-	fdwr = write(fdto, buf, rd);
 	if (fdto == -1 || fdwr == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		exit(99);
+		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
+	fdwr = write(fdto, buf, rd);
+	buf = malloc(sizeof(char) * 1024);
+        rd = read(fdfrom, buf, 1023);
 	closeto = close(fdto);
 	closefrom = close(fdfrom);
 	if (closeto == -1)
