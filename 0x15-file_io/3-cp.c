389a959b6d2359695473091cf31f6a6d5135dadc
ChechG <2100@holbertonschool.com>
2020-11-09 20:03:21 +0000

    0x15

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,9 +26,9 @@ int main(int ac, char **av)
 	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
-	fdwr = write(fdto, buf, rd);
 	buf = malloc(sizeof(char) * 1024);
         rd = read(fdfrom, buf, 1023);
+        fdwr = write(fdto, buf, rd);
 	closeto = close(fdto);
 	closefrom = close(fdfrom);
 	if (closeto == -1)
