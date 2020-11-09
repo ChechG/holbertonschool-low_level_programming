28926d600ba31cf7f8e8982072722af1fe3e8692
ChechG <2100@holbertonschool.com>
2020-11-09 19:08:14 +0000

    0x15

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,17 +23,12 @@ int main(int ac, char **av)
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	if (fdto == -1)
+        rd = read(fdfrom, buf, 1024);
+	fdwr = write(fdto, buf, rd);
+	if (fdto == -1 || fdwr == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
-	}
-	rd = read(fdfrom, buf, 1024);
-	fdwr = write(fdto, buf, rd);
-	if (fdwr == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-                exit(98);
 	}
 	close(fdto);
 	close(fdfrom);
