6c485489365a30f3be9ea25bf37de4135b9aeafc
ChechG <2100@holbertonschool.com>
2020-12-13 23:07:13 +0000

    change 64849

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -15,12 +15,17 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = temp->next;
+		free(temp);
 		return (1);
 	}
-	for (i = 0; temp != NULL && i < index - 1; i++)
+	for (i = 1; temp != NULL && i < index; i++)
 	{
 		temp = temp->next;
 	}
+	if (temp == NULL)
+		return (-1);
+	if (*head == temp)
+		*head = temp->next;
 	if (temp->next != NULL)
 		temp->next->prev = temp->prev;
 	if (temp->prev != NULL)
