dbef989a98c36946ff90f07d5e8bee478b6e1b26
ChechG <2100@holbertonschool.com>
2020-12-13 14:22:54 +0000

    7 idx = 0, before

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,19 +11,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 	dlistint_t *nodo, *new;
 
-	nodo = malloc(sizeof(dlistint_t));
-	nodo->n = n;
-	new = *h;
+	nodo = (listint_t *)malloc(sizeof(listint_t));
 	if (nodo == NULL)
 	{
 		free(nodo);
 		return (NULL);
 	}
+	nodo->n = n;
+	temp = (*head);
 	if (idx == 0)
 	{
 		nodo->n = n;
-		nodo->next = new;
-		new = nodo;
+		nodo->next = (*head);
+		(*head) = nodo;
 		return (nodo);
 	}
 	for (i = 0; i < idx - 1 && new != NULL; i++)
