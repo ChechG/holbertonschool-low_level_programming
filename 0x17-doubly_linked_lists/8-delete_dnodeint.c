f57055d87f10363cae342b101f5026f969a812cc
ChechG <2100@holbertonschool.com>
2020-12-13 22:56:56 +0000

    add with no segm fault

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,24 +11,15 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *temp = *head;
 
 	if (*head == NULL)
-	{
 		return (-1);
-	}
 	if (index == 0)
 	{
 		*head = temp->next;
-		if (temp->next == NULL)
-			(*head)->prev = NULL;
-		free(temp);
 		return (1);
 	}
-	for (i = 0; temp != NULL && i < index; i++)
+	for (i = 0; temp != NULL && i < index - 1; i++)
 	{
 		temp = temp->next;
-	}
-	if (temp == NULL || temp->next == NULL)
-	{
-		return (-1);
 	}
 	if (temp->next != NULL)
 		temp->next->prev = temp->prev;
