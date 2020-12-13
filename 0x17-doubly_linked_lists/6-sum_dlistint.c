6fc3101acc2ef1ccee9ff001452f4ca04a518d4d
ChechG <2100@holbertonschool.com>
2020-12-13 13:54:53 +0000

    betty 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,13 +1,12 @@
 #include "lists.h"
 /**
- * get_dnodeint_at_index - Frees a list.
+ * sum_dlistint - sums the int data of a list.
  * @head: struct dlistint from main.
- * @index: nth node of a linked list.
- * Return: returns the nth node of a linked list.
+ * Return: n.
  */
 int sum_dlistint(dlistint_t *head)
 {
-    int n;
+	int n;
 
 	if (head == NULL)
 	{
