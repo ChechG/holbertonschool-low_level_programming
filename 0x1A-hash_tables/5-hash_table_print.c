73d560bd72b3cae463ab6a8e8d521d5b480b3961
ChechG <2100@holbertonschool.com>
2021-01-15 03:22:31 +0000

    5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -28,7 +28,7 @@ void hash_table_print(const hash_table_t *ht)
 				}
 				else
 				{
-					printf(", '%s': '%s'", ht->array[i]->key);
+					printf(", '%s': ", ht->array[i]->key);
 					printf("'%s'", ht->array[i]->value);
 				}
 			}
