47e71ff427147ba2d56ddb1e140e0dc898bcf689
ChechG <2100@holbertonschool.com>
2021-01-15 05:29:39 +0000

    3 return 1

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(aux->value);
 			aux->value = strdup(value);
-			return (0);
+			return (1);
 		}
 		aux = aux->next;
 	}
