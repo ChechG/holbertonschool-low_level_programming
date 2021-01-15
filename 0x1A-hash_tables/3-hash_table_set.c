120a3aeee2d80b681dd10dcf669e846b758e9e58
ChechG <2100@holbertonschool.com>
2021-01-15 05:18:57 +0000

    errores

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,15 +9,25 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
+	hash_node_t *new_node, *aux;
 	unsigned long int index = key_index((unsigned const char*)key, ht->size);
 
 	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
 		return (0);
+	aux = (ht->array)[index];
+	while (aux != NULL)
+	{
+		if (strcmp(key, aux->key) == 0)
+		{
+			free(aux->value);
+			aux->value = strdup(value);
+			return (0);
+		}
+		aux = aux->next;
+	}
 	new_node = malloc(ht->size * sizeof(hash_node_t *));
 	if (new_node == NULL)
 		return (0);
-	
 	if (ht->array[index] == NULL)
 	{
 		new_node->key = strdup(key);
