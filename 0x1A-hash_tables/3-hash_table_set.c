58d083efec3fbdb51af18bfe775f8d96d8296984
ChechG <2100@holbertonschool.com>
2021-01-15 05:10:32 +0000

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,10 +19,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	if (ht->array[index] != NULL)
+	if (ht->array[index] == NULL)
+		ht->array[index] = new_node;
+	else
 	{
 		new_node->next = ht->array[index];
+		ht->array[index] = new_node;
 	}
-	ht->array[index] = new_node;
 	return (1);
 }
