13c67a611c41db57d81624a8aac36e6ccf667c57
ChechG <2100@holbertonschool.com>
2021-04-20 18:00:58 +0000

    caso borde

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,6 +13,13 @@ int jump_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
+	if (new_size == 1)
+	{
+		if (array[0] == value)
+			return (0);
+		else
+			return(-1);
+	}
 	while (array[mini(step, new_size) - 1] < value)
 	{
 		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
